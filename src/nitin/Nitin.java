7f1c3e95d0676a992ee434b31c05f9a3ad69f1e3
nitin <nitin@nitin-HP-Pavilion-g4-Notebook-PC>
2020-01-11 22:27:57 +0530

    add new java class

---
+++ src/nitin/Nitin.java
@@ -0,0 +1,8 @@
+package nitin;
+public class Nitin
+{
+	public static void main(String[] aaa)
+	{
+		System.out.println("Hello to my java programming language, I love to java programming language, Anyone can not beat me in java programming language");
+	}
+}
